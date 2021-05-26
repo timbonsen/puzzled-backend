0b1406ec66bdee6ca01865d31082081f9f9e87b7
timbonsen <73481594+timbonsen@users.noreply.github.com>
2021-05-26 20:40:20 +0200

    Exchangefunctionaliteiten verwijderd.

--- src/main/java/bonsen/nl/puzzled/repository/ExchangeRepository.java
+++
@@ -1,7 +0,0 @@
-package bonsen.nl.puzzled.repository;
-
-import bonsen.nl.puzzled.model.exchange.Exchange;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExchangeRepository extends JpaRepository<Exchange, String> {
-}
--- src/main/java/bonsen/nl/puzzled/service/exchange/ExchangeService.java
+++
@@ -1,9 +0,0 @@
-package bonsen.nl.puzzled.service.exchange;
-
-import bonsen.nl.puzzled.model.exchange.Exchange;
-
-public interface ExchangeService {
-    public abstract Exchange getExchange(String id);
-    public abstract String createExchange(Exchange exchange);
-    public abstract void deleteExchange(String id);
-}
