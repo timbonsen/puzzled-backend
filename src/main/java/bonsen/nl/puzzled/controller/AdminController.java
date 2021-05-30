5c20a421de2b9f6c64db5d538987de9856fa6600
timbonsen <73481594+timbonsen@users.noreply.github.com>
2021-05-30 13:25:44 +0200

    Overbodige imports verwijderd en ongebruikte maps verwijderd.

--- src/main/java/bonsen/nl/puzzled/controller/AdminController.java
+++ src/main/java/bonsen/nl/puzzled/controller/AdminController.java
@@ -1,14 +1,10 @@
 package bonsen.nl.puzzled.controller;
 
-import bonsen.nl.puzzled.exceptions.BadRequestException;
-import bonsen.nl.puzzled.model.user.User;
 import bonsen.nl.puzzled.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
 import java.util.Set;
 
 @RestController
@@ -19,34 +15,9 @@ public class AdminController {
     @Autowired
     private UserService userService;
 
-    @GetMapping(value = "/")
-    public ResponseEntity<Object> getMessage() {
-        return new ResponseEntity<>("SECURED REST endpoint: /admin", HttpStatus.OK);
-    }
-
     @GetMapping(value = "/all-users")
     public ResponseEntity<Object> getUsers() {
         Set<String> allUsernames = userService.getUsers();
         return ResponseEntity.ok().body(allUsernames);
     }
-
-    @PostMapping(value = "/{username}/authorities")
-    public ResponseEntity<Object> addUserAuthority(@PathVariable("username") String username, @RequestBody Map<String, Object> fields) {
-        try {
-            User user = userService.getUser(username);
-            String authorityName = (String) fields.get("authority");
-            userService.addAuthority(user, authorityName);
-            return ResponseEntity.ok().build();
-        }
-        catch (Exception ex) {
-            throw new BadRequestException();
-        }
-    }
-
-    @DeleteMapping(value = "/{username}/authorities/{authority}")
-    public ResponseEntity<Object> deleteUserAuthority(@PathVariable("username") String username, @PathVariable("authority") String authority) {
-        userService.removeAuthority(username, authority);
-        return ResponseEntity.ok().build();
-    }
-
 }
