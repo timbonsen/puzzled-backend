66263123da329e288294e62796044390cecdc7d7
timbonsen <73481594+timbonsen@users.noreply.github.com>
2021-05-30 16:57:41 +0200

    Setters verwijderd.

--- src/main/java/bonsen/nl/puzzled/model/image/Image.java
+++ src/main/java/bonsen/nl/puzzled/model/image/Image.java
@@ -36,21 +36,12 @@ public class Image {
     public String getName() {
         return name;
     }
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public String getType() {
         return type;
     }
-    public void setType(String type) {
-        this.type = type;
-    }
 
     public byte[] getData() {
         return data;
-    }
-    public void setData(byte[] data) {
-        this.data = data;
     }
 }
